75c1074112fc3c4bef30a0204a83b4b59159cc87
alejandro.perez1 <alejandro.perez1@udea.edu.co>
2021-02-08 00:46:54 -0500

    actualizacion de errores

--- server/index.js
+++ server/index.js
@@ -9,42 +9,62 @@ server.use(cors());
 
 //TODAS LAS TECNOLOGIAS
 server.get('/api/technologies', async (req, res) => {
-    let technologies = await Technology.find(); //Se pone en variable let porque la vamos afectar con la imagen 
-    technologies = technologies.map((technology) => {
+    try {
+        let technologies = await Technology.find(); //Se pone en variable let porque la vamos afectar con la imagen 
+        technologies = technologies.map((technology) => {
         technology.logo = `${req.protocol}://${req.headers.host}/img/${technology.logo}`;
-        return technology;
-    });
-    return res.send({
-        error:false,
-        data: technologies
-    })
+        return technology
+        });
+
+        return res.status(200).json(
+            technologies
+        );
+                
+    } catch (error) {
+        return res.status(404).send({
+            "message": "no se ha encontrado ningún framework con esa ID"
+        });
+    }
 });
 
 server.get('/api/technology/:id', async(req, res) => {
-    const {id} = req.params;
-    let technology = await Technology.findById(id);
-    technology.logo = `${req.protocol}://${req.headers.host}/img/${technology.logo}`;
+    try {
+        const {id} = req.params;
+        let technology = await Technology.findById(id);
+        technology.logo = `${req.protocol}://${req.headers.host}/img/${technology.logo}`;
 
-    return res.send({
-        error: false,
-        data: technology
-    })
+        return res.status(200).json(
+            technology
+        );        
+    } catch (error) {
+        return res.status(404).send({
+            "message": "No se ha encontrado ningún framework con esa ID"
+        });
+    }
+    
 });
 
 server.get('/api/technology/search/:name', async(req, res) => {
-    const {name} = req.params;
-    let technologies = await Technology.find({
-        name: { $regex: new RegExp(name, "i")}
-    });
-    technologies = technologies.map(technology =>{
+    try {
+        const {name} = req.params;
+        let technologies = await Technology.find({
+            name: { $regex: new RegExp(name, "i")}
+        });
+        technologies = technologies.map(technology =>{
         technology.logo = `${req.protocol}://${req.headers.host}/img/${technology.logo}`;
         return technology
     });
+
+    return res.status(200).json(
+        technologies
+    );
+        
+    } catch (error) {
+        return res.status(404).send({
+            "message": "No se ha encontrado ningún framework"
+        });
+    }
     
-    return res.send({
-        error:false,
-        data: technologies
-    });
 });
 
 module.exports = server;
